e2362720b86d59ac784e58bd69195b2c957020e1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-27 22:38:56 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+string PrimeiroNome, Sobrenome;
+
+Console.Write("Digite seu primeiro nome: ");
+PrimeiroNome = Console.ReadLine()!;
+
+Console.Write("Digite seu Sobrenome: ");
+Sobrenome = Console.ReadLine()!;
+
+Console.Write($" Nome Completo: {PrimeiroNome} {Sobrenome}");
+
+Console.WriteLine();
+
+Console.Write($" Nome Catálago: {Sobrenome} {PrimeiroNome}");
